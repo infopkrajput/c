338e14ebf4959d715c918a9a8348ee2b35d22ef1
infopkrajput <p.k.rajput@hotmail.com>
2025-03-06 11:42:56 +0530

    update

--- Class/Vanshika/chapter19/ex.c
+++ Class/Vanshika/chapter19/ex.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    char str[100];
     char ch;
     FILE *f;
-    f = fopen("sample.txt", "wb");
-    printf("Enter a string: ");
-    gets(str);
-    fwrite(str, sizeof(str), 1, f);
+    f = fopen("sample.txt", "r");
+    while (1)
+    {
+        ch = fgetc(f);
+        if(ch == EOF)
+        {
+            break;
+        }
+        printf("%c",ch);
+    } 
     fclose(f);
     return 0;
 }
